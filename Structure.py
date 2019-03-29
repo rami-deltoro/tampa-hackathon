7ece9cfa92ca8a97e2b296b39d85daca3e150427
osnielTAO <osniel.quintana@taoconnect.org>
2019-03-29 10:26:54 -0400

    Refactored stuff.

--- Structure.py
+++ Structure.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 
 class Structure():
   def __init__(self, layer_list):
-
+    print("Creating structure for network using " + layer_list)
 
 class Layer():
     def __init__(self, act_function=sigmoid):
